9a63ff72de14374aea0f08cb152d9311316709e3
Jehiel <jehiel.py@gmail.com>
2024-08-26 20:16:35 +0200

    Refactor game.c para no salir del juego sin agarrar los collectables.

--- src/game.c
+++ src/game.c
@@ -6,7 +6,7 @@
 /*   By: jlinarez <jlinarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 12:49:44 by jlinarez          #+#    #+#             */
-/*   Updated: 2024/08/22 17:27:23 by jlinarez         ###   ########.fr       */
+/*   Updated: 2024/08/26 20:15:20 by jlinarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,12 +40,17 @@ int	handle_key_press(int keycode, t_game *game)
 	return (0);
 }
 
-void	handle_special_cells(char cell, t_game *game)
+static void	handle_special_cells(char cell, t_game *game)
 {
-	if (cell == 'E' && game->collected == game->total_collectables)
+	if (cell == 'E')
 	{
-		ft_printf("¡Has ganado!\n");
-		close_game(game);
+		if (game->collected == game->total_collectables)
+		{
+			ft_printf("¡Has ganado!\n");
+			close_game(game);
+		}
+		else
+			ft_printf("Debes recoger todos los coleccionables antes de salir.\n");
 	}
 	else if (cell == 'C')
 	{
@@ -62,17 +67,20 @@ void	process_movement(int new_x, int new_y, t_game *game)
 	if (new_x < 0 || new_x >= game->map_w
 		|| new_y < 0 || new_y >= game->map_h)
 		return ;
+
 	cell = game->map[new_y][new_x];
-	if (cell != '1')
-	{
-		handle_special_cells(cell, game);
-		game->map[game->player_y][game->player_x] = '0';
-		game->player_x = new_x;
-		game->player_y = new_y;
-		game->map[new_y][new_x] = 'P';
-		game->moves++;
-		ft_printf("Movimientos realizados: %d\n", game->moves);
-		mlx_clear_window(game->mlx, game->win);
-		draw_map(game);
-	}
+	if (cell == '1')
+		return ;
+	handle_special_cells(cell, game);
+	if (cell == 'E' && game->collected != game->total_collectables)
+		return ;
+
+	game->map[game->player_y][game->player_x] = '0';
+	game->player_x = new_x;
+	game->player_y = new_y;
+	game->map[new_y][new_x] = 'P';
+	game->moves++;
+	ft_printf("Movimientos realizados: %d\n", game->moves);
+	mlx_clear_window(game->mlx, game->win);
+	draw_map(game);
 }
