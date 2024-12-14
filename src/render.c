b8973b2c1907a7f8d0208c0eb92e2107b7d32b4c
Jehiel <jehiel.py@gmail.com>
2024-12-14 10:06:29 +0100

    Eliminado codigo innecesario, agregado salto de linea

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: jlinarez <jlinarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 12:49:44 by jlinarez          #+#    #+#             */
-/*   Updated: 2024/08/22 17:20:03 by jlinarez         ###   ########.fr       */
+/*   Updated: 2024/12/14 09:46:58 by jlinarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,54 +43,4 @@ void	draw_map(t_game *game)
 		}
 		y++;
 	}
-}
-
-void	conditional_maps(t_game *game, void *img, int x, int y)
-{
-	if (game->map[y][x] == '1')
-		mlx_put_image_to_window(game->mlx, img,
-			game->wall_texture.img, x * T_SIZE, y * T_SIZE);
-	else if (game->map[y][x] == 'P')
-		mlx_put_image_to_window(game->mlx, img,
-			game->player_texture.img, x * T_SIZE, y * T_SIZE);
-	else if (game->map[y][x] == 'C')
-		mlx_put_image_to_window(game->mlx, img,
-			game->collectable_texture.img, x * T_SIZE, y * T_SIZE);
-	else if (game->map[y][x] == 'E')
-		mlx_put_image_to_window(game->mlx, img,
-			game->exit_texture.img, x * T_SIZE, y * T_SIZE);
-	else
-		mlx_put_image_to_window(game->mlx, img,
-			game->empty_texture.img, x * T_SIZE, y * T_SIZE);
-}
-
-void	draw_map_to_image(t_game *game, void *img)
-{
-	int	x;
-	int	y;
-
-	y = 0;
-	while (y < game->map_h)
-	{
-		x = 0;
-		while (x < game->map_w)
-		{
-			conditional_maps(game, img, x, y);
-			x++;
-		}
-		y++;
-	}
-}
-
-void	render_frame(t_game *game)
-{
-	void	*img;
-
-	img = mlx_new_image(game->mlx, game->map_w * T_SIZE,
-			game->map_h * T_SIZE);
-	if (img == NULL)
-		error_exit("Failed to create new image.");
-	draw_map_to_image(game, img);
-	mlx_put_image_to_window(game->mlx, game->win, img, 0, 0);
-	mlx_destroy_image(game->mlx, img);
 }
--- src/textures.c
+++ src/textures.c
@@ -6,7 +6,7 @@
 /*   By: jlinarez <jlinarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 12:49:44 by jlinarez          #+#    #+#             */
-/*   Updated: 2024/08/19 20:21:51 by jlinarez         ###   ########.fr       */
+/*   Updated: 2024/12/07 16:16:09 by jlinarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ void	load_texture(t_game *game, t_texture *texture, const char *file)
 	if (!texture->img)
 	{
 		ft_printf("Failed to load texture: %s\n", file);
-		error_exit("Error loading texture.");
+		error_exit("Error loading texture.%s\n");
 	}
 }
